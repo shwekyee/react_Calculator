6ee45b0ae08bb10c98187d6859518bf7bda66982
Aung Myo <shwekyee.football@gmail.com>
2022-10-17 14:18:29 +0630

    react_calculator

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+ 
+  return (
+    <div className="calculator-grid">
+     <div className="output">
+        <div className="previous-operand">12312312</div>
+        <div className="current-operand">1231231</div>
+     </div>
+     <button className="span-two">AC</button>
+     <button>DEL</button>
+     <button>-</button>
+     <button>1</button>
+     <button>2</button>
+     <button>3</button>
+     <button>*</button>
+     <button>4</button>
+     <button>5</button>
+     <button>6</button>
+     <button>+</button>
+     <button>7</button>
+     <button>8</button>
+     <button>9</button>
+     <button>-</button>
+     <button>.</button>
+     <button>0</button>
+     <button className="span-two">=</button>
+    </div>
+  )
+}
+
+export default App
